5e90a93b429d1671cf8aea8432669bf393ddd71c
gamb1t <74240139+gam-b1t@users.noreply.github.com>
2021-01-03 01:44:44 +0900

    Create kkutu.user.js

---
+++ kkutu.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         끄투 끝말잇기 핵
+// @version      1.4
+// @namespace    http://tampermonkey.net/
+// @description  끄투코리아 자동 게임 봇입니다.
+// @author       gamb1t
+// @supportURL https://discord.gg/NcGdbDgBMS
+// @homepage https://discord.gg/NcGdbDgBMS
+// @match        https://kkutu.co.kr/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/gam-b1t/kkutu-hack/main/main.js
+// ==/UserScript==
