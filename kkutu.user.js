aa4adc8cbf8f0e4a9ad81a9c443a9aed3325be1c
2Kernel <74240139+2Kernel@users.noreply.github.com>
2021-03-08 01:33:41 +0900

    2Kernel#8231

--- kkutu.user.js
+++ kkutu.user.js
@@ -3,7 +3,7 @@
 // @version      1.4.4
 // @namespace    http://tampermonkey.net/
 // @description  끄투 끝말잇기 자동완성 봇
-// @author       2Kernel
+// @author       2Kernel#8231
 // @supportURL https://discord.gg/Kj4h7KkDMn
 // @homepage https://discord.gg/Kj4h7KkDMn
 // @iconURL https://i.imgur.com/oKHDoxH.png
