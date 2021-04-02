d2455ab87b0bfe0b29b0dd4049cc77fba71d43e1
gamb1t <74240139+gamb2t@users.noreply.github.com>
2021-04-02 09:19:41 +0900

    Update kkutu.user.js

--- kkutu.user.js
+++ kkutu.user.js
@@ -3,11 +3,9 @@
 // @version      1.4.4
 // @namespace    http://tampermonkey.net/
 // @description  끄투 끝말잇기 자동 완성 봇
-// @author       2Kernel#8231
-// @supportURL https://discord.gg/Kj4h7KkDMn
-// @homepage https://discord.gg/Kj4h7KkDMn
+// @author       gamb1t#0001
 // @iconURL https://i.imgur.com/oKHDoxH.png
 // @match        https://kkutu.co.kr/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/2Kernel/kkutu-hack/main/main.js
+// @require      https://raw.githubusercontent.com/gamb2t/kkutu-hack/main/main.js
 // ==/UserScript==
